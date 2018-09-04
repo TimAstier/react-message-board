3524dbacc40bf1d89576a49d39333e8a94bc40ed
Tim <timothee.astier@essec.edu>
2018-09-04 11:08:23 +0200

    First implementation of <Message />

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,17 +1,86 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Message = props => {
+const Wrapper = styled.div`
+  width: 250px;
+  min-height: 100px;
+  background-color: ${props => props.isChildMessage ? '#f1fab3' : '#d5f013'};
+  display: flex;
+`;
+
+const LeftSidebar = styled.div`
+  min-width: 35px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const RightSidebar = styled.div`
+  min-width: 35px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+const Text = styled.div`
+  flex-grow: 1;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
+
+const MessageButton = styled.div`
+  cursor: pointer;
+`;
+
+const Message = ({ text, belongsToCurrentUser, isChildMessage }) => {
   return (
-    <div>Hey</div>
+    <Wrapper isChildMessage={isChildMessage}>
+      <LeftSidebar>
+        <MessageButton>
+          &#8618;
+        </MessageButton>
+      </LeftSidebar>
+      <Text>
+        {text}
+      </Text>
+      <RightSidebar>
+        {
+          belongsToCurrentUser &&
+          <Fragment>
+            <MessageButton>
+              <span role="img" aria-label="wastebasket emoji">
+                🗑️
+              </span>
+            </MessageButton>
+            <MessageButton>
+              &#9998;
+            </MessageButton>
+          </Fragment>
+        }
+      </RightSidebar>
+    </Wrapper>
   );
 };
 
 Message.propTypes = {
-  id: PropTypes.number.isRequired,
+  // id: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
-  parentId: PropTypes.number,
-  authorId: PropTypes.number.isRequired,
+  respond: PropTypes.func.isRequired,
+  belongsToCurrentUser: PropTypes.bool,
+  delete: PropTypes.func,
+  edit: PropTypes.func,
+  isChildMessage: PropTypes.bool
+  // parentId: PropTypes.number,
+  // authorId: PropTypes.number.isRequired,
 };
 
 export default Message;
--- src/components/Message/stories.js
+++ src/components/Message/stories.js
@@ -4,4 +4,36 @@ import { storiesOf } from '@storybook/react';
 import Message from './Message';
 
 storiesOf('Message', module)
-  .add('with no props', () => <Message />)
+  .add('short text', () =>
+    <Message
+      text="Hello world!"
+    />
+  )
+  .add('medium text', () =>
+    <Message
+      text="This is our new message board! I'm so excited."
+    />
+  )
+  .add('long text', () =>
+    <Message
+      text={`
+        This is such a long text that I have no idea how it can fit
+        into a single message but we will see how it goes. It needs
+        to be even longer to break the minimum height so I'm adding
+        even more meaningless text.
+      `}
+    />
+  )
+  .add('belongsToCurrentUser', () =>
+    <Message
+      text="This is my own message."
+      belongsToCurrentUser
+    />
+  )
+  .add('isChildMessage', () =>
+    <Message
+      text="Hello world!"
+      isChildMessage
+    />
+  )
+  ;
