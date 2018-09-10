a78ca94b636583c8374fded6dd16829253d848a2
Tim <timothee.astier@essec.edu>
2018-09-10 20:09:25 +0200

    Re-add renamed folder

---
+++ src/Textarea/stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Textarea from './Textarea';
+
+storiesOf('Textarea', module)
+  .add("text: ''", () => (
+    <Textarea
+      value=""
+      handleChange={() => console.log('change!')}
+    />
+  ))
+  .add('long text', () => (
+    <Textarea
+      value={'#'.repeat(240)}
+      handleChange={() => console.log('change!')}
+    />
+  ))
+  .add('noBorder: true', () => (
+    <Textarea
+      value="This Textarea has no border."
+      handleChange={() => console.log('change!')}
+      noBorder={true}
+    />
+  ))
+  .add('autoFocus: true', () => (
+    <Textarea
+      value=""
+      autoFocus={true}
+      handleChange={() => console.log('change!')}
+    />
+  ));
